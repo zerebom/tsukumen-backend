862d06d45e1f658ec5cd3f00a19f6de501c3bddd
zerebom <ice.choco.pudding.kokoro@gmail.com>
2020-11-23 11:18:29 +0900

    rails db:migrate.

--- db/migrate/20201123011052_create_shops.rb
+++ db/migrate/20201123011052_create_shops.rb
@@ -1,6 +1,9 @@
 class CreateShops < ActiveRecord::Migration[6.0]
   def change
     create_table :shops do |t|
+      t.string :name
+      t.string :phone_number
+      t.string :email
 
       t.timestamps
     end
