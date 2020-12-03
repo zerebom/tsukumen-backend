1cb88a9572957626d1c036daf9e45ec1412cd6e9
zerebom <ice.choco.pudding.kokoro@gmail.com>
2020-12-03 22:09:03 +0900

    Add get_logger.

---
+++ inject-gcp-data/infra/logger.py
@@ -0,0 +1,32 @@
+from logging import getLogger, StreamHandler, FileHandler, Formatter
+
+def get_logger(name, log_level='DEBUG',
+               output_file=None,
+               handler_level='INFO',
+               output_level='DEBUG',
+               format_str='%(message)s'):
+    """
+    :param str name:
+    :param str log_level:
+    :param str | None output_file:
+    :return: logger
+    """
+    logger = getLogger(name)
+
+    formatter = Formatter(format_str)
+
+    handler = StreamHandler()
+    logger.setLevel(log_level)
+    handler.setLevel(handler_level)
+    handler.setFormatter(formatter)
+
+    logger.handlers = []
+    logger.addHandler(handler)
+
+    if output_file:
+        file_handler = FileHandler(output_file)
+        file_handler.setFormatter(formatter)
+        file_handler.setLevel(output_level)
+        logger.addHandler(file_handler)
+
+    return logger
