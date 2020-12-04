8eeb59f8fe8a7a08307be41b809ada27724bb73f
zerebom <ice.choco.pudding.kokoro@gmail.com>
2020-12-04 09:59:29 +0900

    Fix reviews_controller logic.

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -3,13 +3,18 @@ module Api
     module V1
         class ReviewsController < ApplicationController
             def index
-                review = Review.all
-                render json:review
+                shop = Shop.find(params[:shop_id])
+                reviews = shop.reviews.all
+                render json:{ status: 'SUCCESS', message: 'Loaded the reviews', data: reviews }
             end
 
             def show
-                @review = Review.find(params[:id])
-                render json: { status: 'SUCCESS', message: 'Loaded the review', data: @review }
+                all_reviews = Shop.find(params[:shop_id]).reviews
+
+                review_id = params[:id].to_i
+                # 0-index->1-index
+                review = all_reviews[review_id-1]
+                render json: { status: 'SUCCESS', message: 'Loaded the review', data:review}
             end
         end
     end
