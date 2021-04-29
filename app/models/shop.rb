f690fc5db377e1cf782c8140c048d63389e78e32
zerebom <ice.choco.pudding.kokoro@gmail.com>
2021-04-29 10:41:07 +0900

    del comment & change schema.

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -15,15 +15,3 @@ class Shop < ApplicationRecord
     self
   end
 end
-
-#   # ref: https://qiita.com/suzuki-koya/items/1553c405beeb73f83bbc
-#   def self.from_result(result:,
-#                        place_id:)
-#     shop = self.new
-#     shop.name = result['name']
-#     shop.phone_number = result['formatted_phone_number']
-#     shop.email = result['email']
-#     shop.place_id = place_id
-#     shop
-#   end
-# end
