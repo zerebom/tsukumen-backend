e2f7b2d28d4bca94e0def8813ffcecad65e38ba0
zerebom <ice.choco.pudding.kokoro@gmail.com>
2021-04-29 13:12:44 +0900

    add review spec.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
-
 class Review < ApplicationRecord
   belongs_to :shop
+
+  def convert_result_to_kwargs(result:, place_id:)
+    kwargs = {}
+    name_corespondence = {'author_name': 'reviewer',
+                              'text': 'review_text',
+                              'rating': 'star'}
+    kwargs = {}
+      name_corespondence.each do |gcp_name, db_name|
+        kwargs[db_name.to_sym] = result[db_name]
+    end
+
+  def from_result(result:, place_id:)
+    kwargs = self.convert_result_to_kwargs(result, place_id)
+    self.reviewer = kwargs['reviewer']
+    self.star = kwargs['star']
+    self.review_text = kwargs['review_text']
+    self
+  end
+
 end
