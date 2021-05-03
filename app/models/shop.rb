02e0254692d5caa49f4bc15205feda686ce5bedf
zerebom <ice.choco.pudding.kokoro@gmail.com>
2021-05-03 19:18:12 +0900

    add photos

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -12,7 +12,7 @@ class Shop < ApplicationRecord
     self.phone_number = result['formatted_phone_number']
     self.email = result['email']
     self.place_id = place_id
-    self.opening_hours = result['opening_hours']['weekday_text'].join(',')
+    self.opening_hours = result['opening_hours']['weekday_text'].join(',') if result.key?('opening_hours')
     self
   end
 end
